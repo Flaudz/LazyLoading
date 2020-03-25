2e2ef0abdec6be3a9698b7ac1bc455a8df0891cc
Flaudz <nico936d@edu.campusvejle.dk>
2020-03-25 11:11:54 +0100

    Added some code in script.js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-const imgs = document.querySelectorAll("img");
-
-imgs.forEach((img, index) => {
-	img.setAttribute(
-		"src",
-		`https://picsum.photos/200/300?random=${index.toString()}`
-	);
-});
+if ("loading" in HTMLImageElement.prototype) {
+	const images = document.querySelectorAll("img");
+	images.forEach((img) => {
+		img.setAttribute("height", "300px");
+		img.setAttribute("width", "200px");
+		img.src = img.dataset.src;
+		img.removeAttribute("data-src");
+	});
+} else {
+	const script = document.createElement("script");
+	script.src =
+		"https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.8/lazysizes.min.js";
+	document.body.appendChild(script);
+}
